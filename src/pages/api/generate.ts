746a4fb00a37159db184ebd4855e6b94c6d5d3e4
Azad-Aihub <128484356+Azad-Aihub@users.noreply.github.com>
2024-11-29 22:25:33 +0800

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -4,7 +4,7 @@ import { verifySignature } from '@/utils/auth'
 const demoKey = import.meta.env.DEMOKEY;
 import prompts from "@/prompts"
 
-const baseUrl = 'https://api.wochirou.com';
+const baseUrl = 'https://api.cymru';
 
 
 
