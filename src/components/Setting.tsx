a6693e986a443c160555251ce54be0bf9211aa28
Azad-Aihub <128484356+Azad-Aihub@users.noreply.github.com>
2025-03-08 09:18:18 +0800

    Update Setting.tsx

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -65,28 +65,6 @@ export default function Setting(props: {
           />
         </SettingItem>
       </Show>
-
-      <button onclick="exportToMarkdown()">导出为Markdown</button>
-    <script>
-        function exportToMarkdown() {
-            // 获取要导出的HTML内容
-            const content = document.getElementById('content').innerHTML;
-            // 创建Turndown实例
-            const turndownService = new TurndownService();
-            // 将HTML转换为Markdown
-            const markdown = turndownService.turndown(content);
-            // 创建Blob对象
-            const blob = new Blob([markdown], { type: 'text/markdown' });
-            // 创建下载链接
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = 'export.md';
-            // 触发下载
-            link.click();
-        }
-    </script>
-
-
     </div>
   )
 }
